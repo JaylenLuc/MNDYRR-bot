5dfc3b2df019705c3e1590373ed9da36e6b6e7f6
JaylenLuc <JHLUC@UCI.EDU>
2024-02-15 15:52:28 -0800

    changes to hyperparameters

--- AI_backend/AI_Logic/main.py
+++ AI_backend/AI_Logic/main.py
@@ -9,16 +9,20 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core import documents
 from langchain_core.documents import Document
+load_dotenv()
+#ASTRADB KEYS
+ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")#CHANGE IF DATABASE COLLECTION CHANGES
+ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT") #CHANGE IF DATABASE COLLECTION CHANGES
+#OPENAI KEYS
+OPEN_AI_API_KEY = os.environ.get("OPENAI_API_KEY")
+#ASTRADB COLLECTION NAME
+ASTRA_DB_COLLECTION = os.environ.get("ASTRA_DB_COLLECTION") #CHANGE IF DATABASE COLLECTION CHANGES
+
+OPEN_AI_TEMP = 0.8
+
+CONTEXT_COUNT = 5
 
 def start_RAG():
-    load_dotenv()
-    #ASTRADB KEYS
-    ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")#CHANGE IF DATABASE COLLECTION CHANGES
-    ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT") #CHANGE IF DATABASE COLLECTION CHANGES
-    #OPENAI KEYS
-    OPEN_AI_API_KEY = os.environ.get("OPENAI_API_KEY")
-    #ASTRADB COLLECTION NAME
-    ASTRA_DB_COLLECTION = os.environ.get("ASTRA_DB_COLLECTION") #CHANGE IF DATABASE COLLECTION CHANGES
     print("KEY CONFIG DONE")
 
     embedding = OpenAIEmbeddings()
@@ -30,6 +34,19 @@ def start_RAG():
     )
     print("DB STORE DONE")
 
+    prompt_template = """
+    Answer the question using the supplied context at your own discretion. If you don't know the answer, give your best answer.
+    Context: {context}
+    Question: {question}
+    Your answer:
+    """
+
+    model = ChatOpenAI(openai_api_key=OPEN_AI_API_KEY, temperature=OPEN_AI_TEMP)
+
+    return [vstore,prompt_template,model]
+
+
+def populate_db(vstore, dataset):
     philo_dataset = load_dataset("datastax/philosopher-quotes")["train"]
     print("An example entry:")
     print(philo_dataset[16])
@@ -51,21 +68,12 @@ def start_RAG():
     print(f"\nInserted {len(inserted_ids)} documents.")
     #print(vstore.astra_db.collection(ASTRA_DB_COLLECTION).find_one())
     print(vstore.astra_db.collection(ASTRA_DB_COLLECTION).count_documents())
-    prompt_template = """
-    Answer the question using the supplied context at your own discretion. If you don't know the answer, give your best answer.
-    Context: {context}
-    Question: {question}
-    Your answer:
-    """
 
-    model = ChatOpenAI(openai_api_key=OPEN_AI_API_KEY)
 
-    return [vstore,prompt_template,model]
-
-def get_response(vstore,prompt_template,model,context_int,message):
-    print(vstore,prompt_template,model,context_int,message)
+def get_response(vstore,prompt_template,model,message):
+    print(vstore,prompt_template,model,CONTEXT_COUNT,message)
     prompt = ChatPromptTemplate.from_template(prompt_template)
-    context_retr = vstore.as_retriever(search_kwargs={'k': context_int})
+    context_retr = vstore.as_retriever(search_kwargs={'k': CONTEXT_COUNT})
     chain = (
         {"context": context_retr, "question": RunnablePassthrough()}
         | prompt
--- AI_backend/RAG/views.py
+++ AI_backend/RAG/views.py
@@ -4,8 +4,8 @@ import AI_Logic.main as AI
 # Create your views here.
 #this is to start the connection and build the AI 
 AI_PACK = AI.start_RAG()
-
+AI.populate_db(AI_PACK[0], None)
 def get_ai_response(req):
-    resp = AI.get_response(*AI_PACK,3,req.GET.get("question"))
+    resp = AI.get_response(*AI_PACK,req.GET.get("question"))
     print(resp)
     return JsonResponse({"response":resp})
