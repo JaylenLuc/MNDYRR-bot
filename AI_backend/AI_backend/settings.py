9d1f7a2a0d605644b861953606bd6cbc47e96fa8
JaylenLuc <JHLUC@UCI.EDU>
2024-05-13 17:46:34 -0700

    fd

--- AI_backend/AI_backend/settings.py
+++ AI_backend/AI_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
