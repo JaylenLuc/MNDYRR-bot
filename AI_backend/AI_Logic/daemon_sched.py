c4890f589cf2f2ade1b85805f397306cb4d79eff
JaylenLuc <JHLUC@UCI.EDU>
2024-05-20 20:48:10 -0700

    daemon scheduler

---
+++ AI_backend/AI_Logic/daemon_sched.py
@@ -0,0 +1,27 @@
+def send_astra():
+    import os
+    from dotenv import load_dotenv
+    from langchain_community.vectorstores.astradb import AstraDB
+    from langchain_openai import OpenAIEmbeddings
+    import time
+    load_dotenv()
+    ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")#CHANGE IF DATABASE COLLECTION CHANGES
+    ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT") #CHANGE IF DATABASE COLLECTION CHANGES
+    #OPENAI KEYS
+    OPEN_AI_API_KEY = os.getenv("OPENAI_API_KEY")
+    #ASTRADB COLLECTION NAME
+    ASTRA_DB_COLLECTION = os.getenv("ASTRA_DB_COLLECTION") #CHANGE IF DATABASE COLLECTION CHANGES
+    ASTRA_DB_COLLECTION_ONE = os.getenv("ASTRA_DB_COLLECTION_ONE")
+    embedding = OpenAIEmbeddings(api_key=OPEN_AI_API_KEY)
+    while (True):
+        print("send")
+        vstore = AstraDB(
+            embedding=embedding,
+            collection_name=ASTRA_DB_COLLECTION_ONE,
+            token=ASTRA_DB_APPLICATION_TOKEN,
+            api_endpoint=ASTRA_DB_API_ENDPOINT,
+        )
+        time.sleep(144000.0)
+    return
+if __name__ == "__main__":
+    send_astra()
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn AI_backend.AI_backend.wsgi --bind 0.0.0.0:$PORT
+worker: python AI_backend/AI_Logic/daemon_sched.py
